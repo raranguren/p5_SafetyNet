891edb150355e03f8e1fb4f6fb44369d9ab1234b
ricar <ricaragas@hotmail.com>
2022-08-01 16:58:35 +0200

    Now actuator httptrace is enabled by implementing HttpTraceRepository that also generates logs

---
+++ src/main/java/com/ricaragas/safetynet/repository/HttpTraceRepositoryImpl.java
@@ -0,0 +1,31 @@
+package com.ricaragas.safetynet.repository;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.actuate.trace.http.HttpTrace;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@Log4j2
+public class HttpTraceRepositoryImpl implements HttpTraceRepository {
+
+    private HttpTrace lastTrace;
+
+    // Return only the last HTTP trace when requested in actuator
+    @Override
+    public List<HttpTrace> findAll() {
+        if (lastTrace == null) return List.of();
+        return List.of(lastTrace);
+    }
+
+    // Log the trace at INFO level
+    @Override
+    public void add(HttpTrace trace) {
+        lastTrace = trace;
+        var request = trace.getRequest();
+        var response = trace.getResponse();
+        log.info("HTTP Request: {} {} --> Response: status={} {}", request.getMethod(), request.getUri(), response.getStatus(), response.getHeaders());
+    }
+}
